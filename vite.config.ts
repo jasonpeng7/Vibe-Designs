2186bfe058c07a0c80707f4a915aefac7c98caaa
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-09-07 15:35:53 -0700

    sitemap and postcss plug in

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import tailwindcss from "tailwindcss";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    sitemap({
+      hostname: "https://www.vbedigital.com",
+      dynamicRoutes: ["/", "/privacy-policy", "/terms-of-service"],
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
