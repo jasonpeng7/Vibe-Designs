4f47dd2b173c568faba3a8b9be31b1a066fe64c0
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-09-07 16:28:30 -0700

    uninstall site map

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "tailwindcss";
-import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: "https://www.vbedigital.com",
-      dynamicRoutes: ["/", "/privacy-policy", "/terms-of-service"],
-    }),
-  ],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
