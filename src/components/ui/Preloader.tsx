5e589c2e85b78025e28f84bb11e943aa6961e9ba
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-09-07 20:02:05 -0700

    removed preloader component

--- src/components/ui/Preloader.tsx
+++
@@ -1,9 +0,0 @@
-const Preloader = () => {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="w-16 h-16 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-};
-
-export default Preloader;
