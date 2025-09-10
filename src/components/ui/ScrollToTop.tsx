649f346fa6140cb604c96c4aad64104af741e75f
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-09-10 16:16:11 -0700

    scroll function

---
+++ src/components/ui/ScrollToTop.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Disable the browser's automatic scroll restoration
+    if (window.history.scrollRestoration) {
+      window.history.scrollRestoration = "manual";
+    }
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollToTop;
