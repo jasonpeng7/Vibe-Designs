45175cf3111c75993646abf898f9f9a5512d1210
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-09-10 19:42:41 -0700

    added mobile images + updated info

--- src/lib/portfolio-data.ts
+++ src/lib/portfolio-data.ts
@@ -2,7 +2,7 @@ export const portfolioData = [
   {
     slug: "peng-flooring",
     client: "Peng Flooring",
-    title: "Modernizing a Local Leader",
+    title: "Modernizing a Local Business",
     details: {
       tier: "Launch",
       cost: "$900",
@@ -15,6 +15,7 @@ export const portfolioData = [
         layers: [
           {
             src: "/peng-flooring.png",
+            mobileSrc: "/peng-floor-mobile.jpg",
             alt: "Screenshot of the Peng Flooring website landing section.",
             parallax: 1.0,
           },
@@ -22,10 +23,11 @@ export const portfolioData = [
       },
       {
         title: "About Page",
-        caption: "A page that tells your brand's story.",
+        caption: "A page that tells the business' story.",
         layers: [
           {
             src: "/peng-floor-about.png",
+            mobileSrc: "/peng-floor-about-mobile.jpg",
             alt: "Screenshot of the Peng Flooring about section.",
             parallax: 1.0,
           },
@@ -37,6 +39,7 @@ export const portfolioData = [
         layers: [
           {
             src: "/peng-floor-service.png",
+            mobileSrc: "/peng-floor-service-mobile.jpg",
             alt: "Screenshot of the services section of the Peng Flooring website.",
             parallax: 1.0,
           },
@@ -44,11 +47,11 @@ export const portfolioData = [
       },
       {
         title: "Localization",
-        caption:
-          "Localization of the website for many target audiences around the world.",
+        caption: "Localization of website to target different languages.",
         layers: [
           {
             src: "/peng-floor-localization.png",
+            mobileSrc: "/peng-floor-localization-mobile.jpg",
             alt: "Screenshot of the localization section of the Peng Flooring website.",
             parallax: 1.0,
           },
@@ -60,8 +63,9 @@ export const portfolioData = [
         layers: [
           {
             src: "/peng-floor-contact.png",
+            mobileSrc: "/peng-floor-booking-mobile.jpg",
             alt: "Screenshot of the contact form on the Peng Flooring website.",
-            parallax: 1.0,
+            parallax: 1.01,
           },
         ],
       },
@@ -70,9 +74,9 @@ export const portfolioData = [
       problem:
         "Peng Flooring is a local flooring business that didn't have online presence, let alone a website. They could not reflect their quality of work or services and was not attracting online leads.",
       solution:
-        "We designed and developed a modern website with a strong focus user conversion and experience. We highlighted their portfolio, clarified their services, and implemented a clear call-to-action strategy to convert visitors into customers.",
+        "We designed and developed a modern website with a strong focus on user conversion and experience. We highlighted their portfolio, clarified their services, and implemented a clear call-to-action strategy to convert visitors into customers.",
       outcome:
-        "The new website saw a 1,500 daily requests and a 64% increase in online inquiries within the first three months and significantly boosted their local search engine ranking.",
+        "The new website had 1,500 monthly requests on average and a 64% increase in online inquiries within the first three months and significantly boosted their local search engine ranking.",
     },
   },
   {
@@ -80,7 +84,7 @@ export const portfolioData = [
     client: "Atharva",
     title: "Engineer's Digital Showcase",
     details: {
-      tier: "Custom - SPA",
+      tier: "Custom - Single Page",
       cost: "$200",
       timeline: "3 days",
     },
@@ -91,18 +95,32 @@ export const portfolioData = [
         layers: [
           {
             src: "/atharva-portfolio.png",
+            mobileSrc: "/atharva-mobile.jpg",
             alt: "Screenshot of Atharva's portfolio hero section.",
             parallax: 1.0,
           },
         ],
       },
       {
-        title: "Project Showcase",
+        title: "Projects Showcase",
+        caption: "Professional and informative landing page.",
+        layers: [
+          {
+            src: "/atharva-projects.png",
+            mobileSrc: "/atharva-projects-mobile.jpg",
+            alt: "Screenshot of Atharva's projects section.",
+            parallax: 1.0,
+          },
+        ],
+      },
+      {
+        title: "Specific Project Page",
         caption: "Detailed project cards with clear descriptions.",
         layers: [
           {
             src: "/atharva-project.png",
-            alt: "Screenshot of the project section.",
+            mobileSrc: "/atharva-specific-project-mobile.jpg",
+            alt: "Screenshot of the specific project.",
             parallax: 1.0,
           },
         ],
